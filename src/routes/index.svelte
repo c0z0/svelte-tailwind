5923a701fa50d26e3e19d1aa5970b0a398138999
Cosmin Serdean <cosmin@cserdean.com>
2021-05-21 17:22:45 +0300

    testing

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,34 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import { onMount } from 'svelte';
+
+	let inverted = false;
+
+	let counter = 0;
+
+	const onClick = () => {
+		inverted = !inverted;
+
+		counter += 1;
+	};
+
+	onMount(() => {
+		const interval = setInterval(onClick, 1000);
+
+		return () => clearInterval(interval);
+	});
+</script>
+
+<svelte:head>
+	<title>Lollio</title>
+</svelte:head>
+
+<div>Count: {counter}</div>
+
+<div class="container mx-auto mt-96 select-none" on:click={onClick}>
+	<h1 class="text-9xl font-heading text-center text-gray-700 cursor-pointer">
+		Welcome to <span
+			class="font-bold text-indigo-500 transition-all duration-1000"
+			class:text-pink-500={inverted}>Lollio</span
+		>
+	</h1>
+</div>
---
+++ tailwind.config.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+	mode: 'jit',
+	purge: ['./src/**/*.{html,js,svelte,ts}'],
+	theme: {
+		extend: {},
+		fontFamily: {
+			heading: ['Noto Sans', 'sans-serif']
+		}
+	},
+	plugins: []
+};
